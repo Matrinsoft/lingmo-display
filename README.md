1b4f9681c64150000d85e5e94c0ebe5ccc247af3
Lingmo <lingmo@lingmo.org>
2026-07-10 18:42:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# lingmo-display
+
+Lingmo Display
+
+## Build
+
+`ash
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+make -j
+`
+
+## Dependencies
+
+- Qt6 >= 6.5
+- C++20
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <QtCore/qglobal.h>
+
+#if defined(LINGMO_LINGMO_DISPLAY_LIBRARY)
+#  define LINGMO_LINGMO_DISPLAY_EXPORT Q_DECL_EXPORT
+#else
+#  define LINGMO_LINGMO_DISPLAY_EXPORT Q_DECL_IMPORT
+#endif
